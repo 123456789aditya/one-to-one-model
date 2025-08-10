48f2c02f8937a81b8b76d0852426ab713613a163
aditya pathak <pathakaditya881@gmail.com>
2025-08-10 23:46:53 +0530

    added

--- project/app/models.py
+++ project/app/models.py
@@ -6,7 +6,7 @@ class Aadhar(models.Model):
     create_date=models.DateField(max_length=20)
     created_by=models.CharField(max_length=20)
     
-class Student(models.Model):
-    stu_name=models.CharField(max_length=20)
-    stu_contact=models.IntegerField(max_length=20)
-    stu_aadharno=models.OneToOneField(Aadhar,on_delete=models.PROTECT,to_field="aadhar")
+# class Student(models.Model):
+#     stu_name=models.CharField(max_length=20)
+#     stu_contact=models.IntegerField(max_length=20)
+#     stu_aadharno=models.OneToOneField(Aadhar,on_delete=models.PROTECT,to_field="aadhar")
