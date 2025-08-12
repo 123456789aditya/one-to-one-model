62ce041ac645308ecec211308929c1899ae3232e
aditya pathak <pathakaditya881@gmail.com>
2025-08-12 23:41:05 +0530

    added

--- project/app/models.py
+++ project/app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# # Create your models here.
-# class Aadhar(models.Model):
-#     aadhar=models.IntegerField(max_length=20,unique=True)
-#     create_date=models.DateField(max_length=20)
-#     created_by=models.CharField(max_length=20)
+# Create your models here.
+class Aadhar(models.Model):
+    aadhar=models.IntegerField(max_length=20,unique=True)
+    create_date=models.DateField(max_length=20)
+    created_by=models.CharField(max_length=20)
     
-# class Student(models.Model):
-#     stu_name=models.CharField(max_length=20)
-#     stu_contact=models.IntegerField(max_length=20)
-#     stu_aadharno=models.OneToOneField(Aadhar,on_delete=models.PROTECT,to_field="aadhar")
+class Student(models.Model):
+    stu_name=models.CharField(max_length=20)
+    stu_contact=models.IntegerField(max_length=20)
+    stu_aadharno=models.OneToOneField(Aadhar,on_delete=models.PROTECT,to_field="aadhar")
